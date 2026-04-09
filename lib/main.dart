c88e36d99eb84df445e145e35b54253350850399
Wang Yihan <218583@student.upm.edu.my>
2026-04-09 11:25:12 +0800

    end

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'next_page/next_page.dart';
 void main() {
   runApp(MyApp());
 }
@@ -14,10 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyAppExt(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyAppExt());
   }
 }
 
@@ -29,79 +26,80 @@ class MyAppExt extends StatefulWidget {
 }
 
 class _MyAppExtState extends State<MyAppExt> {
-    String buttoName = 'Click';
+  String buttoName = 'Click';
   int currentIndex = 0;
+  bool _isClicked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text('APP Title'),
-        ),
-        body: Center(
-          child: currentIndex == 0
-              ? Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.red,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.red,
-                          backgroundColor: Colors.orange,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            buttoName = 'Clicked';
-                          });
-                        },
-                        child: Text(buttoName),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('APP Title'),
+      ),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.red,
+                        backgroundColor: Colors.orange,
                       ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (BuildContext context) =>
-                                  const NextPage(),
-                            ),
-                          );
-                        },
-                        child: const Text('Next Page'),
+                      onPressed: () {
+                        setState(() {
+                          buttoName = 'Clicked';
+                        });
+                      },
+                      child: Text(buttoName),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => const NextPage(),
+                          ),
+                        );
+                      },
+                      child: const Text('Next Page'),
+                    ),
+                  ],
+                ),
+              )
+            : GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isClicked = !_isClicked;
+                  });
+                },
+                child: _isClicked
+                    ? Image.asset('images/FLAGG.jpg')
+                    : Image.network(
+                        'https://statico.profootballnetwork.com/wp-content/uploads/2025/10/23153116/cooper-flaggs-heartwarming-gesture-goes-10-23-25-3840x2449.jpg',
                       ),
-                    ],
-                  ),
-                )
-              : Image.asset('images/FLAGG.jpg'),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
-            BottomNavigationBarItem(
-              label: 'Settings',
-              icon: Icon(Icons.settings),
-            ),
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
-      );
+              ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+            label: 'Settings',
+            icon: Icon(Icons.settings),
+          ),
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
 
-
-
-class NextPage extends StatelessWidget {
-  const NextPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
-  }
-}
